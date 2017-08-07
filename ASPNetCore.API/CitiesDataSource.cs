e1304aa85b9fe9d4f05bee037d91c21b884d6cda
Ziaullah Khan <khan.ziaullah@gmail.com>
2017-08-07 14:39:58 +0530

    Added model and controller

---
+++ ASPNetCore.API/CitiesDataSource.cs
@@ -0,0 +1,22 @@
+﻿using ASPNetCore.API.Model;
+using System.Collections.Generic;
+
+namespace ASPNetCore.API
+{
+    public class CitiesDataSource
+    {
+        public static CitiesDataSource Current = new CitiesDataSource();
+
+        public List<CityDto> Cities;
+
+        public CitiesDataSource()
+        {
+            Cities = new List<CityDto>
+            {
+                new CityDto { Id = 1, Name = "New York", Description = "", NumberOfTouristSpots = 20},
+                new CityDto { Id = 2, Name = "Columbus", Description = "", NumberOfTouristSpots = 2},
+                new CityDto { Id = 3, Name = "Chicago", Description = "", NumberOfTouristSpots = 10},
+            };
+        }
+    }
+}
---
+++ ASPNetCore.API/Controller/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNetCore.API.Model;
+
+namespace ASPNetCore.API.Controller
+{
+    [Route("api/cities")]
+    public class CitiesController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet]
+        public List<CityDto> GetCities()
+        {
+            return CitiesDataSource.Current.Cities;
+        }
+
+        [HttpGet("{id}")]
+        public CityDto GetCity(int id)
+        {
+            return CitiesDataSource.Current.Cities.Where(x=> x.Id == id).First();
+        }
+    }
+}
